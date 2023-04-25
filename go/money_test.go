84538d1fe323f11a1525b3dcb2e1250976d2c0a4
litmus-zhang <abdulsalamlukmon9@gmail.com>
2023-04-25 21:19:09 +0100

    chapter 3 done

--- go/money_test.go
+++ go/money_test.go
@@ -29,6 +29,21 @@ func TestDivision(t *testing.T) {
 	assertEqual(t, expectedMoneyAfterDivision, actualMoneyAfterDivision)
 }
 
+func TestAddition(t *testing.T) {
+	var portfolio Portfolio
+	var portfolioInDollars Money
+
+	fiveDollars := Money{amount: 5, currency: "USD"}
+	tenDollars := Money{amount: 10, currency: "USD"}
+	fifteenDollars := Money{amount: 15, currency: "USD"}
+
+	portfolio = portfolio.Add(fiveDollars)
+	portfolio = portfolio.Add(tenDollars)
+	portfolioInDollars = portfolio.Evaluate("USD")
+
+	assertEqual(t, fifteenDollars, portfolioInDollars)
+}
+
 func assertEqual(t *testing.T, expected Money, actual Money) {
 	if expected != actual {
 		t.Errorf("Expected %v, got %v", expected, actual)
@@ -38,6 +53,20 @@ func assertEqual(t *testing.T, expected Money, actual Money) {
 type Money struct {
 	amount   float64
 	currency string
+}
+type Portfolio []Money
+
+func (p Portfolio) Add(money Money) Portfolio {
+	p = append(p, money)
+	return p
+}
+
+func (p Portfolio) Evaluate(currency string) Money {
+	total := 0.0
+	for _, m := range p {
+		total += m.amount
+	}
+	return Money{amount: total, currency: currency}
 }
 
 func (m Money) Times(multiplier int) Money {
