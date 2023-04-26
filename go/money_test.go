d46f03189237825f09e3fa31ff1388055a4145d0
litmus-zhang <abdulsalamlukmon9@gmail.com>
2023-04-26 15:54:47 +0000

    Chapter 12 complted

--- go/money_test.go
+++ go/money_test.go
@@ -8,7 +8,7 @@ import (
 
 var bank s.Bank
 
-func init() {
+func initExchangeRates() {
 	bank = s.NewBank()
 	bank.AddExchangeRate("EUR", "USD", 1.2)
 	bank.AddExchangeRate("USD", "KRW", 1100)
@@ -42,6 +42,7 @@ func TestAddition(t *testing.T) {
 }
 
 func TestAdditionOfDollarAndEuros(t *testing.T) {
+	initExchangeRates()
 	var portfolio s.Portfolio
 
 	fiveDollars := s.NewMoney(5, "USD")
@@ -59,6 +60,7 @@ func TestAdditionOfDollarAndEuros(t *testing.T) {
 }
 
 func TestAdditionOfDollarAndWons(t *testing.T) {
+	initExchangeRates()
 	var portfolio s.Portfolio
 
 	oneDollar := s.NewMoney(1, "USD")
@@ -74,6 +76,7 @@ func TestAdditionOfDollarAndWons(t *testing.T) {
 }
 
 func TestAdditionWithMultipleMissingExchangeRates(t *testing.T) {
+	initExchangeRates()
 	var portfolio s.Portfolio
 
 	oneDollar := s.NewMoney(1, "USD")
@@ -92,13 +95,17 @@ func TestAdditionWithMultipleMissingExchangeRates(t *testing.T) {
 	assertEqual(t, expectedErrorMessage, actualError.Error())
 
 }
-func TestConversion(t *testing.T) {
-	bank := s.NewBank()
-	bank.AddExchangeRate("EUR", "USD", 1.2)
+func TestConversionWithDifferentRatesBetweenTwoCurrencies(t *testing.T) {
+	initExchangeRates()
 	tenEuros := s.NewMoney(10, "EUR")
 	actualConvertedMoney, err := bank.Convert(tenEuros, "USD")
 	assertNil(t, err)
 	assertEqual(t, s.NewMoney(12, "USD"), *actualConvertedMoney)
+
+	bank.AddExchangeRate("EUR", "USD", 1.3)
+	actualConvertedMoney, err = bank.Convert(tenEuros, "USD")
+	assertNil(t, err)
+	assertEqual(t, s.NewMoney(13, "USD"), *actualConvertedMoney)
 }
 
 func TestConversionWithMissingExchangeRate(t *testing.T) {
